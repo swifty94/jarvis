91abcad47486e4c7b43749248dd8ec6dd5811745
master <master@devnull.local>
2020-09-12 16:40:08 +0300

    upd selfcheck

--- bin/jarvis.sh
+++ bin/jarvis.sh
@@ -10,7 +10,7 @@
 export JARVIS_HOME="$(pwd)"
 export now=`date +%m_%d_%H:%M:%S`
 
-start() {
+function start() {
 
         if [[ ! -d $JARVIS_HOME/log ]]; then
                 mkdir $JARVIS_HOME/log
@@ -96,7 +96,7 @@ start() {
         fi
 }
 
-stop() {
+function stop() {
         PID1=$(ps aux|grep -v grep|grep jarvis.py |awk '{print $2}')
         PID2=$(ps aux|grep -v grep|grep sql_worker.py |awk '{print $2}')
         echo "Jarvis server: gracefull shutdown."
@@ -113,10 +113,59 @@ stop() {
         echo ''
 }       
 
-restart() {
+function restart() {
         stop
         sleep 5
         start
+}
+
+function check(){
+    URL='http://127.0.0.1:5000'
+    ENDPOINTS=(
+    "/"
+    "dashboard"
+    "cpu_stats"
+    "ram_stats"
+    "net_stats"
+    "disk_stats"
+    "processes"
+    )
+    for i in "${ENDPOINTS[@]}"; do
+        echo "HTTP request to $i endpoint"
+        response=$(curl -s -I $URL/$i | grep HTTP/1.1 | awk {'print $2'})
+        echo $response >> tmp.log
+        echo "HTTP response from $i - $response"
+        echo ''
+    done
+    INTSERVER=$(grep -ic 500 tmp.log)
+    NOTFOUND=$(grep -ic 404 tmp.log)
+    NOPERMISSIONS=$(grep -ic 403 tmp.log)
+    OK=$(grep -ic 200 tmp.log)
+    echo "Score:"
+    echo "HTTP 200: $OK"
+    echo "HTTP 500: $INTSERVER"
+    echo "HTTP 404: $NOTFOUND"
+    echo "HTTP 403: $NOPERMISSIONS"
+    if [ $NOPERMISSIONS -ne 0 ]; then
+        echo "Result:"
+        echo "HTTP ERROR while hook to UI"
+        echo "Post-deploy tests FAILED"
+        exit 1
+    elif [ $NOTFOUND -ne 0 ]; then
+        echo "Result:"
+        echo "HTTP ERROR while hook to UI"
+        echo "Post-deploy tests FAILED"
+        exit 1
+    elif [ $INTSERVER -ne 0 ]; then
+        echo "Result:"
+        echo "HTTP ERROR while hook to UI"
+        echo "Post-deploy tests FAILED"
+    else
+        echo "Result:"
+        echo "UI hooks OK"
+        echo "Post-deploy tests passed"
+    fi
+    rm -f tmp.log    
 }
 
 case "$1" in
@@ -129,12 +178,15 @@ case "$1" in
         restart)
                 restart
                 ;;
+        check)
+                check
+                ;;
         *)
                 echo "Jarvis server: NOTE: usage example:"
                 echo ''
                 echo "user@host:~/jarvis_home ./jarvis start"
                 echo ''
-                echo "available options: start, stop, restart"
+                echo "available options: start, stop, restart, check"
                 exit 1
 esac
 
